057b6a43ae47daf8191b093c9a1477fdd7db1a59
Daniel dos Santos <denieldossantoos@gmail.com>
2022-04-18 06:32:04 -0300

    New feature

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -16,6 +16,13 @@ public class Jogo {
 		
 		monstro.atacar(heroi);
 		heroi.atacar(monstro);
+		
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		monstro.andar(Direcao.NORTE);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
 
 		System.out.println("Monstro tem =>" + monstro.vida);
 		System.out.println("Heroi tem =>" + heroi.vida);
