6a8f84d067f34e773dc6ba12d1cfb598a427e043
Daniel dos Santos <denieldossantoos@gmail.com>
2022-07-06 07:24:38 -0300

    lambdas

---
+++ src/oo/abstrato/Cachorro.java
@@ -0,0 +1,15 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero {
+
+	@Override
+	public String mover() {
+		// TODO Stub de método gerado automaticamente
+		return "Usando as patas";
+	}
+	@Override
+	public String mamar() {
+		// TODO Stub de método gerado automaticamente
+		return "Usando leite";
+	}
+}
