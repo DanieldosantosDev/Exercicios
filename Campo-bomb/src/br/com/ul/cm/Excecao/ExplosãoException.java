5331bd3b34195af3af6553a8b0cd5da562d83e3a
Daniel dos Santos <denieldossantoos@gmail.com>
2022-12-11 15:33:12 -0300

    new updates with Junit

---
+++ Campo-bomb/src/br/com/ul/cm/Excecao/ExplosãoException.java
@@ -0,0 +1,8 @@
+package br.com.ul.cm.Excecao;
+
+public class ExplosãoException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+
+}
--- Campo-bomb/src/br/com/ul/cm/modelo/Campo.java
+++ Campo-bomb/src/br/com/ul/cm/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.ul.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.ul.cm.Excecao.ExplosãoException;
+
 public class Campo {
 	private final int linha;
 	private final int coluna;
@@ -37,7 +39,81 @@ public class Campo {
 	} else { 
 		return false;
 	}
+}
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	boolean abrir() {
+		if(!aberto && !marcado){
+		aberto = true;
+		if(minado) {
+			throw new ExplosãoException();
+			
+		}
+		if (vizinhancaSegura()) {
+			vizinhos.forEach(v -> v.abrir());
+			
+		}
+		return true;
+	} else {
+		return false;
+		}
+	}
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+	void minar() {
+		minado = true;
+	}
+	public boolean isMarcado() {
+		return marcado;
+	}
+	public boolean isAberto() {
+		return aberto;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && !aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+	long minasNaVizinhanca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+		}
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	
+	}
+	public String toString() {
+		if(marcado) {
+			return "x";
+		}
+		else if(aberto && minado) {
+			return "*";
+		}
+		else if(aberto && minasNaVizinhanca() > 0) {
+			return Long.toString(minasNaVizinhanca());
+		}
+		else if(aberto) {
+			return  " ";
+		}
+		else {
+			return "?";
+		}
 	}
 }
+
 
 
